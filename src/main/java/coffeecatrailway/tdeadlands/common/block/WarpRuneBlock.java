cdf9f15e680d83add32d582f21790acda8be774b
CoffeeCatRailway <dragonlee1002@gmail.com>
2020-08-24 08:49:32 +1000

    Warp Rune is now water loggable

--- src/main/java/coffeecatrailway/tdeadlands/common/block/WarpRuneBlock.java
+++ src/main/java/coffeecatrailway/tdeadlands/common/block/WarpRuneBlock.java
@@ -1,8 +1,11 @@
 package coffeecatrailway.tdeadlands.common.block;
 
+import io.github.ocelot.common.BaseBlock;
+import io.github.ocelot.common.VoxelShapeHelper;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.block.IWaterLoggable;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.BlockItemUseContext;
@@ -14,6 +17,9 @@ import net.minecraft.state.properties.BlockStateProperties;
 import net.minecraft.state.properties.DoubleBlockHalf;
 import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.shapes.ISelectionContext;
+import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.world.IBlockReader;
 import net.minecraft.world.IWorld;
 import net.minecraft.world.IWorldReader;
 import net.minecraft.world.World;
@@ -24,14 +30,28 @@ import javax.annotation.Nullable;
  * @author CoffeeCatRailway
  * Created: 23/08/2020
  */
-public class WarpRuneBlock extends Block {
+public class WarpRuneBlock extends BaseBlock implements IWaterLoggable {
 
     public static final BooleanProperty POWERED = BlockStateProperties.POWERED;
     public static final EnumProperty<DoubleBlockHalf> HALF = BlockStateProperties.DOUBLE_BLOCK_HALF;
 
+    private static final VoxelShape LOWER = new VoxelShapeHelper.Builder().append(
+            Block.makeCuboidShape(2d, 0d, 2d, 14d, 13d, 14d),
+            Block.makeCuboidShape(4d, 13d, 4d, 12d, 16d, 12d)
+    ).build();
+    private static final VoxelShape UPPER = new VoxelShapeHelper.Builder().append(
+            Block.makeCuboidShape(4d, 0d, 4d, 12d, 8d, 12d),
+            Block.makeCuboidShape(6d, 8d, 6d, 10d, 16d, 10d)
+    ).build();
+
     public WarpRuneBlock(Properties properties) {
         super(properties);
-        this.setDefaultState(this.stateContainer.getBaseState().with(POWERED, false).with(HALF, DoubleBlockHalf.LOWER));
+        this.setDefaultState(this.stateContainer.getBaseState().with(POWERED, false).with(HALF, DoubleBlockHalf.LOWER).with(WATERLOGGED, false));
+    }
+
+    @Override
+    public VoxelShape getShape(BlockState state, IBlockReader world, BlockPos pos, ISelectionContext context) {
+        return state.get(HALF) == DoubleBlockHalf.LOWER ? LOWER : UPPER;
     }
 
     @Override
@@ -59,7 +79,7 @@ public class WarpRuneBlock extends Block {
 
     @Override
     protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
-        builder.add(POWERED, HALF);
+        builder.add(POWERED, HALF, WATERLOGGED);
     }
 
     @Override
@@ -76,7 +96,7 @@ public class WarpRuneBlock extends Block {
         BlockPos pos = context.getPos();
         if (pos.getY() < context.getWorld().getHeight() - 1)
             if (context.getWorld().getBlockState(pos.up()).isReplaceable(context))
-                return this.getDefaultState().with(POWERED, false);
+                return super.getStateForPlacement(context).with(POWERED, false);
         return null;
     }
 
