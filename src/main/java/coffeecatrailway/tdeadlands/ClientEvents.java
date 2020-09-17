bee84b0f2976822996cd4330c6410779ee49afde
CoffeeCatRailway <dragonlee1002@gmail.com>
2020-09-17 11:37:12 +1000

    Fixed spawn egg colors

--- src/main/java/coffeecatrailway/tdeadlands/ClientEvents.java
+++ src/main/java/coffeecatrailway/tdeadlands/ClientEvents.java
@@ -1,9 +1,12 @@
 package coffeecatrailway.tdeadlands;
 
 import coffeecatrailway.tdeadlands.client.entity.renderer.DeadWoodArrowRenderer;
+import coffeecatrailway.tdeadlands.client.entity.renderer.MuskOxRenderer;
 import coffeecatrailway.tdeadlands.client.entity.renderer.OwlRenderer;
 import coffeecatrailway.tdeadlands.client.entity.renderer.RatRenderer;
 import coffeecatrailway.tdeadlands.registry.DeadEntities;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.color.ItemColors;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.fml.client.registry.RenderingRegistry;
@@ -16,7 +19,15 @@ import org.apache.logging.log4j.Logger;
 @OnlyIn(Dist.CLIENT)
 public class ClientEvents
 {
-    private static final Logger LOGGER = TheDeadlands.getLogger("Client Events");
+    private static final Logger LOGGER = TheDeadlands.getLogger("Client-Events");
+
+    public static void itemColors()
+    {
+        ItemColors itemColors = Minecraft.getInstance().getItemColors();
+        DeadEntities.SPAWN_EGGS.forEach(egg -> itemColors.register((stack, tintindex) -> egg.get().getColor(tintindex), egg.get()));
+
+        LOGGER.info("Item colors");
+    }
 
     public static void entityRenderers()
     {
