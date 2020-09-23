efd64dfccb5a95b37aeeb81cdd9669f152c0ec91
CoffeeCatRailway <dragonlee1002@gmail.com>
2020-09-23 14:51:08 +1000

    Added frost bronze ingot jei info

--- src/main/java/coffeecatrailway/tdeadlands/integration/jei/DeadJEIPlugin.java
+++ src/main/java/coffeecatrailway/tdeadlands/integration/jei/DeadJEIPlugin.java
@@ -1,13 +1,22 @@
 package coffeecatrailway.tdeadlands.integration.jei;
 
 import coffeecatrailway.tdeadlands.TheDeadlands;
+import coffeecatrailway.tdeadlands.integration.registrate.DeadTags;
 import coffeecatrailway.tdeadlands.registry.DeadBlocks;
+import coffeecatrailway.tdeadlands.registry.DeadItems;
+import com.google.common.collect.Lists;
 import mezz.jei.api.IModPlugin;
 import mezz.jei.api.JeiPlugin;
 import mezz.jei.api.constants.VanillaRecipeCategoryUid;
+import mezz.jei.api.constants.VanillaTypes;
 import mezz.jei.api.registration.IRecipeCatalystRegistration;
+import mezz.jei.api.registration.IRecipeRegistration;
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
+
+import java.util.List;
 
 /**
  * @author CoffeeCatRailway
@@ -25,8 +34,28 @@ public class DeadJEIPlugin implements IModPlugin
     }
 
     @Override
+    public void registerRecipes(IRecipeRegistration reg)
+    {
+        StringBuilder builder = new StringBuilder();
+        List<Block> convertBlocks = DeadTags.Blocks.FROST_BRONZE_CONVERT.getAllElements();
+        int count = convertBlocks.size() - 1;
+        for (int i = 0; i < count; i++)
+        {
+            builder.append(convertBlocks.get(i).getTranslatedName().getString()).append(i < count - 1 ? ", " : "");
+        }
+        builder.append(" or ").append(convertBlocks.get(count).getTranslatedName().getString());
+        List<ItemStack> ingotStacks = Lists.newArrayList(new ItemStack(DeadItems.FROST_COPPER_CLUMP.get()), new ItemStack(DeadItems.FROST_BRONZE_INGOT.get()));
+        reg.addIngredientInfo(ingotStacks, VanillaTypes.ITEM,
+                "A " + ingotStacks.get(1).getDisplayName().getString() + " can be made by throwing a " + ingotStacks.get(0).getDisplayName().getString() +
+                        " into " + builder.toString() + ".",
+                "You will get the same amount back that you through in.");
+    }
+
+    @Override
     public void registerRecipeCatalysts(IRecipeCatalystRegistration reg)
     {
         reg.addRecipeCatalyst(new ItemStack(DeadBlocks.DEAD_WOOD_CRAFTING_TABLE.get()), VanillaRecipeCategoryUid.CRAFTING);
+        reg.addRecipeCatalyst(new ItemStack(DeadItems.FROST_BRONZE_INGOT.get()), VanillaRecipeCategoryUid.INFORMATION);
+        reg.addRecipeCatalyst(new ItemStack(DeadItems.FROST_COPPER_CLUMP.get()), VanillaRecipeCategoryUid.INFORMATION);
     }
 }
